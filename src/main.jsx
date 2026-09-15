ed78a531b6dd7a51d3aac7c5e5ebfdc564568516
Parichay Sahani <124479425+Pari100@users.noreply.github.com>
2026-09-15 22:30:51 +0530

    Revert "Fix mobile navigation and loading spinner"
    This reverts commit bef36faf5cb86b94388e7d9f3c7353407ac14c46.

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
-import { StrictMode, useEffect, useState } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Spinner  from './components/spinner'
-
-function Root() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-
-
-const timeout = setTimeout(() => setIsLoading(false), 10000);
-    return () => clearTimeout(timeout);
-  }, []);
-
-  return isLoading ? <Spinner /> : <App />;
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Root />
+    <App />
   </StrictMode>,
 )
